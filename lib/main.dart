a2f169659322cd6b630a8b8b76407f04f4e92062
Tembeon <tembeon@gmail.com>
2022-07-15 13:37:38 +0300

    migrate BlocObserver to the latest bloc version

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-import 'screens/app/app_observer.dart';
-import 'screens/app/view/app.dart';
+import 'app/view/app.dart';
+import 'app_observer.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy(); // remove # from url
-  Bloc.observer = AppObserver();
-  runApp(const FindAnimeApp());
+void main() {
+  setPathUrlStrategy();
+
+  BlocOverrides.runZoned(
+    () => runApp(const FindAnimeApp()),
+    blocObserver: AppObserver(),
+  );
 }

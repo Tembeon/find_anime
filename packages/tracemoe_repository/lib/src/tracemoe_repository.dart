0309b102d9ff00a3bbd19ac108db00089f8f7ae2
Tembeon <tembeon@gmail.com>
2022-07-24 15:21:48 +0300

    [tracemoe_repository] add support for `getResultByFile` method

--- packages/tracemoe_repository/lib/src/tracemoe_repository.dart
+++ packages/tracemoe_repository/lib/src/tracemoe_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:tracemoe_api/tracemoe_api.dart';
 import 'package:tracemoe_repository/src/models/result.dart';
 
@@ -10,36 +12,51 @@ class TraceMoeRepository {
   final TraceMoeApiClient _apiClient;
 
   Future<List<Result>> getResultByImageUrl(String url) async {
-    SearchResultModel? test;
     try {
       final response = await _apiClient.getResultByImageUrl(url);
-      test = response;
-
-      if (response.result != null && response.result!.isNotEmpty) {
-        List<Result> resultList = [];
-
-        for (ResultModel result in response.result!) {
-          resultList.add(
-            Result(
-              similarity: result.similarity,
-              episode: result.episode ?? 0,
-              moment: result.from ?? 0,
-              video: result.video!,
-              idMal: result.anilist!.idMal!,
-              adultOnly: result.anilist!.isAdult ?? false,
-              romajiName: result.anilist!.title!.romaji,
-              japaneseName: result.anilist!.title!.native,
-            ),
-          );
-        }
-
-        return resultList;
-      } else {
-        throw EmptyResultFailure();
-      }
+      return _getResultList(response);
     } catch (e) {
-      print('ERROR: $e\n\n RESPONSE: $test');
       rethrow;
+    }
+  }
+
+  Future<List<Result>> getResultByFile(
+    Uint8List bytes, {
+    required String mimeType,
+  }) async {
+    try {
+      final response = await _apiClient.getResultByFile(
+        bytes,
+        mimeType: mimeType,
+      );
+      return _getResultList(response);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  List<Result> _getResultList(SearchResultModel model) {
+    if (model.result != null && model.result!.isNotEmpty) {
+      List<Result> resultList = [];
+
+      for (ResultModel result in model.result!) {
+        resultList.add(
+          Result(
+            similarity: result.similarity,
+            episode: result.episode ?? 0,
+            moment: result.from ?? 0,
+            video: result.video!,
+            idMal: result.anilist!.idMal!,
+            adultOnly: result.anilist!.isAdult ?? false,
+            romajiName: result.anilist!.title!.romaji,
+            japaneseName: result.anilist!.title!.native,
+          ),
+        );
+      }
+
+      return resultList;
+    } else {
+      throw EmptyResultFailure();
     }
   }
 }

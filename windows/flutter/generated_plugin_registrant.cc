9a8a049089b9d1ae21871aad5c69cacecdb79212
Tembeon <tembeon@gmail.com>
2022-08-17 15:57:49 +0300

    add windows support

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <pasteboard/pasteboard_plugin.h>
+#include <url_launcher_windows/url_launcher_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PasteboardPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PasteboardPlugin"));
+  UrlLauncherPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+}
